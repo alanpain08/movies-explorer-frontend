8a7af850abe9be57b1598d046472d8c3acace6f9
Alan Tsarakov <alantsarakov@yandex.ru>
2021-12-21 23:08:10 +0300

    level-3

---
+++ src/utils/MainApi.js
@@ -0,0 +1,79 @@
+import { apiConfig } from './constants';
+
+class MainApi {
+  constructor({ adress, headers }) {
+    this._adress = adress;
+    this._headers = headers;
+  }
+
+  _checkServerAnswer(res) {
+    if (res.ok) {
+      return res.json();
+    }
+
+    return Promise.reject(`Что-то пошло не так: ${res.status}`);
+  }
+
+  getUserInfo() {
+    return fetch(`${this._adress}/users/me`, {
+      headers: this._headers,
+      credentials: 'include',
+    }).then((res) => this._checkServerAnswer(res));
+  }
+
+  editUserInfo({ name, email }) {
+    return fetch(`${this._adress}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      credentials: 'include',
+      body: JSON.stringify({
+        name: name,
+        email: email,
+      }),
+    }).then((res) => this._checkServerAnswer(res));
+  }
+
+  getInitialCards() {
+    return fetch(`${this._adress}/cards`, {
+      headers: this._headers,
+      credentials: 'include',
+    }).then((res) => this._checkServerAnswer(res));
+  }
+
+  putLike(cardId) {
+    return fetch(`${this._adress}/cards/${cardId}/likes`, {
+      method: 'PUT',
+      headers: this._headers,
+      credentials: 'include',
+    }).then((res) => this._checkServerAnswer(res));
+  }
+
+  deleteLike(cardId) {
+    return fetch(`${this._adress}/cards/${cardId}/likes`, {
+      method: 'DELETE',
+      headers: this._headers,
+      credentials: 'include',
+    }).then((res) => this._checkServerAnswer(res));
+  }
+
+  deleteCard(cardId) {
+    return fetch(`${this._adress}/cards/${cardId}`, {
+      method: 'DELETE',
+      headers: this._headers,
+      credentials: 'include',
+    }).then((res) => this._checkServerAnswer(res));
+  }
+
+  editAvatar({ avatar }) {
+    return fetch(`${this._adress}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this._headers,
+      credentials: 'include',
+      body: JSON.stringify({
+        avatar: avatar,
+      }),
+    }).then((res) => this._checkServerAnswer(res));
+  }
+}
+
+export const mainApi = new MainApi(apiConfig);
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const apiConfig = {
+  adress: `https://api.minikinopoisk.nomoredomains.rocks`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+export const BASE_URL = 'https://api.minikinopoisk.nomoredomains.rocks';
