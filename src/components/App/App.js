e08c46a496b17fa11cb815040dd7ba0f97365bea
Alan Tsarakov <alantsarakov@yandex.ru>
2022-01-11 16:35:52 +0300

    level-3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -52,7 +52,6 @@ function App() {
         localStorage.removeItem('searchedMovies');
         localStorage.removeItem('allMovies');
         setLoggedIn(false);
-        navigate('/')
       })
   }, [navigate]);
 
