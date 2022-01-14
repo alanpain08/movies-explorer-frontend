f6ca232cd456adcc5ef49943f2d55a1209255bea
Alan Tsarakov <alantsarakov@yandex.ru>
2022-01-14 15:49:32 +0300

    level-3

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,13 @@
 export const API_CONFIG = {
-  adress: `http://localhost:3000`,
+  adress: `https://api.minikinopoisk.nomoredomains.rocks`,
   headers: {
     'Content-Type': 'application/json',
   },
 };
 
-//https://api.minikinopoisk.nomoredomains.rocks
+//
 
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://api.minikinopoisk.nomoredomains.rocks';
 
 export const BASE_MOVIE_URL = ' https://api.nomoreparties.co/beatfilm-movies';
 
